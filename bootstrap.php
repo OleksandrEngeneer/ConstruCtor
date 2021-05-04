3ac62add6b1cfe717a72cec91b5db3f666b107c7
OleksandrEngeneer <83660225+OleksandrEngeneer@users.noreply.github.com>
2021-05-04 23:23:14 +0300

    Delete bootstrap.php

--- bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../App/parametrs/configs.php';
-require_once __DIR__ . '/../App/functions/functions.php';
-require_once __DIR__ . '/../App/routes.php';
-require_once __DIR__ . '/../view/view.php';
